9a26097876d1d43686e4b201ba470f4268b268fc
Gen <archiherewego@gmail.com>
2015-05-12 22:21:41 +0900

    コードのクリーンアップ +ログを出力するだけのオーバーライドしたメソッドの削除 +コメントの削除

--- app/src/main/java/jp/gcreate/sample/asynctaskloadersample/MyAsyncTaskLoader.java
+++ app/src/main/java/jp/gcreate/sample/asynctaskloadersample/MyAsyncTaskLoader.java
@@ -6,33 +6,9 @@ import android.os.OperationCanceledException;
 import android.util.Log;
 
 /**
- * 非同期処理をキャンセルできるようにする。
- * 問題点：restartLoaderを呼ぶと、以前の非同期処理が終わらないとrestartLoaderした非同期処理が始まらない。
- * （restartLoaderを2回連続で呼び出す（rerun AsyncTaskLoaderボタンを連続で押す）とよく分かる）
- * restartLoaderしたらすぐに新しい非同期処理が始まって欲しい。
- * （使わない処理結果が出るまで待つのは、時間とリソースのムダ使いである）
+ * AsyncTaskLoaderで途中経過の通知を行う
  *
- * loadInBackground内でキャンセルされているかどうかをチェックするようにし、キャンセルされていたら処理を止めるようにする。
- *
- * ActivityからforceLoadを直接呼ぶとLoaderの非同期処理（loadInBackground）が強制的に動き出す。
- * その後でrestartLoaderを呼ぶとActivityからforceLoadした非同期処理はキャンセルされない。
- * これはLoaderManagerの管理外からLoaderを直接触ったせいで、LoaderManagerからすると対象のLoaderは動いていることになっていないからである。
- * つまり、LoaderはLoaderManagerを介さずにActivityから直接操作するべきではないということになる。
- *
- * ActivityからforceLoadした場合、forceLoadした瞬間にActivityにonLoadFinishedにLoaderManagerから処理結果が通知される。
- * これはinitLoaderを呼び出した際に、対象のLoaderが処理終了している場合にcallOnLoadFinishedを呼ぶように実装されているからである。
- * その後、Loaderに対して直接forceLoadを呼び出したことによって非同期処理が走るが、処理結果はActivityに対して通知しない。
- * これはLoaderManagerは対象のLoaderの処理結果を既に保持しているため、処理結果をわざわざ再通知することをしないのである。
- * （Loaderが非同期処理の結果、毎回異なるデータを返すようなものの場合にはActivityに通知される。
- * 　処理結果が前のものと異なっているからである。）
- *
- * 最終的に、AsyncTaskLoaderは以下のように実装すればよい。
- * + 処理結果のキャッシュを持たせる
- * ++ deliverResultで処理結果をキャッシュに保存する
- * ++ deliverResultではisResetのチェック、isStartedの場合は処理結果を返すように実装する
- * ++ onStartLoadingでキャッシュがある場合はキャッシュをdeliverResultで渡す
- * + loadInBackgroundでキャンセルされているかをチェックし、非同期処理が止まるようにする
- * + リソースの解放が必要な場合、onResetで実装する
+ * LocalBroadCastを利用するバージョン
  */
 public class MyAsyncTaskLoader extends AsyncTaskLoader<String> {
     private static final String TAG = "MyAsyncTaskLoader";
@@ -52,12 +28,6 @@ public class MyAsyncTaskLoader extends AsyncTaskLoader<String> {
     @Override
     public String loadInBackground() {
         for (int i = 0; i < mCount; i++) {
-            //loadInBackgroundがキャンセルされているかチェックする
-            //キャンセルした結果はActivityには通知されない
-            //データ型を処理結果と合わせて適当なもの（nullでも可）を返すか、
-            //OperationCanceledExceptionを投げる（AsyncTaskLoaderの内部にあるAsyncTaskがこれをキャッチして処理が止まる）
-            //いずれにせよ、loadInBackground内で、isLoadInBackgroundCanceledを使ってキャンセルされているかを
-            //チェックしなければならない
             if(isLoadInBackgroundCanceled()){
                 Log.d(TAG, this + " loadInBackground canceled and throw OperationCanceledException.");
                 throw new OperationCanceledException();
@@ -77,9 +47,7 @@ public class MyAsyncTaskLoader extends AsyncTaskLoader<String> {
         Log.d(TAG, this + " onStartLoading." + dumpState());
         if(mCachedResult != null){
             Log.d(TAG, this + " has cached result:" + mCachedResult + dumpState());
-            //キャッシュデータがある場合はキャッシュデータを返す
             deliverResult(mCachedResult);
-            //非同期処理をする必要が無いため処理を抜ける
             return;
         }
         Log.d(TAG, this + " forceLoad." + dumpState());
@@ -89,94 +57,22 @@ public class MyAsyncTaskLoader extends AsyncTaskLoader<String> {
     @Override
     public void deliverResult(String data) {
         Log.d(TAG, this + " deliverResult :" + data + dumpState());
-        //Loaderがお役御免になっている場合（resetされたLoaderは2度と使われない・・・のだと思う）
-        //LoaderManagerがLoaderをresetした後にここに来る可能性はある（非同期処理があまりにも長い場合とか？）
         if(isReset()){
             destroy();
             return;
         }
-        //処理結果をキャッシュに入れる
+
         Log.d(TAG, this + " saved results to cache." + dumpState());
         mCachedResult = data;
 
-        //Loaderが開始状態なら処理結果を通知する
         if(isStarted()) {
             Log.d(TAG, this + " delivers results." + dumpState());
             super.deliverResult(data);
         }
     }
 
-    @Override
-    protected void onReset() {
-        //LoaderManager.restartLoaderを呼んで、新しいアクティブなLoaderの処理が終わったら、古いLoaderの
-        //これが呼ばれている。
-        //再利用されるのかされないのかがいまいちはっきりしなくて気持ち悪い。
-        Log.d(TAG, this + " onReset. This loader will never used." + dumpState());
-        destroy();
-    }
-
-    @Override
-    public void reset() {
-        Log.d(TAG, this + " reset before to call super.reset()" + dumpState());
-        super.reset();
-        Log.d(TAG, this + " reset after super.reset()" + dumpState());
-    }
-
     private void destroy(){
         mCachedResult = null;
-    }
-
-    @Override
-    protected void onAbandon() {
-        //LoaderManager.restartLoaderを呼んだら、古いLoaderのこれが呼ばれる
-        Log.d(TAG, this + " onAbandon." + dumpState());
-    }
-
-    @Override
-    protected void onStopLoading() {
-        //メソッドをオーバーライドしただけでは呼ばれない
-        //その場合はActivityがonStop()になったときには呼ばれている（画面回転は除く）
-        Log.d(TAG, this + " onStopLoading()." + dumpState());
-    }
-
-    @Override
-    protected boolean onCancelLoad() {
-        //Loaderの非同期処理が実行されている時で、次のような場合に呼ばれる
-        //+ Loaderが非同期処理実行中の間に、Activity等でinitLoader().forceLoad()をしたとき
-        //+ Activity等でrestartLoaderを呼んだ時（非同期処理が実行中かは問わない）
-        Log.d(TAG, this + " onCancelLoad()." + dumpState());
-        //AsyncTaskLoader.onCancelLoad()でキャンセル関連の処理が行われているためsuper.onCancelLoad()を呼ぶ
-        //ただしやってるのはLoaderの管理情報の更新するだけ
-        //具体的には、、現在実行中の非同期処理があるか確認（mTask != null）
-        //→ある場合：キャンセル処理中の非同期処理があるか確認(mCancellingTask != null)
-        //　→ある場合：onCancelLoadが呼び出されたLoaderが次に実行する予定のタスクなら破棄する(mTask.waiting == true)
-        //　　（実行待ちのLoaderをキャンセルするということで、実行待ちのタスクはまだ開始されてないから破棄するだけでいい）
-        //　→ない場合：onCancelLoadが呼び出されたLoaderが次に実行予定のタスクか確認
-        //　　→mTask.waiting == true：Loaderを破棄
-        //　　→false：現在実行中の非同期処理をキャンセル処理中のタスクへ移動(mCancellingTask = mTask)
-        //　　その上でcancelLoadInBackgroundを呼び出す
-        //→ない場合：キャンセル対象がないので何もしない
-        //実際のキャンセル処理はcancelLoadInBackgroundで実装する
-        return super.onCancelLoad();
-    }
-
-    @Override
-    public void cancelLoadInBackground() {
-        //onCancelLoad()が呼ばれた後、Loaderが今実行中の非同期処理であればこれが呼ばれる
-        //ここでloadInBackgroundで動いている処理を実際に止める処理を行えばよいはず
-        //どちらかというと、メインスレッド（Activity）から呼び出して非同期処理を止めるためのメソッド
-        Log.d(TAG, this + " cancelLoadInBackground()." + dumpState());
-        //super.cancelLoadInBackground() は何も実装されていないので呼ぶ必要なし
-    }
-
-    @Override
-    public void onCanceled(String data) {
-        //onCancelLoad()が呼ばれたタスクが終了し、deliverResultが呼ばれた時にここにくる
-        //引数は非同期処理が終了した結果（途中経過ではない）
-        //Activityへはこの結果は通知されない
-        //非同期処理の結果がcloseが必要だったりするのであれば、ここでリソースを解放する
-        Log.d(TAG, this + " onCanceled with result:" + data + dumpState());
-        //super.onCanceled(data) は何もしていないため呼ぶ必要なし
     }
 
     private String dumpState() {
